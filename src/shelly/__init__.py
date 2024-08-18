eac14e9e4976d8eb89e3a81431342223f618db29
Jesse Harwin <jesse@jharwin.com>
2024-08-18 13:43:05 -0700

    added __all__

--- src/shelly/__init__.py
+++ src/shelly/__init__.py
@@ -5,3 +5,5 @@ This package contains modules related to interacting with Shelly devices,
 including the main control and MQTT interaction.
 """
 from .dimmer2 import Dimmer2
+
+__all__ = ["Dimmer2"]
