45cb2d45ffbb0ce3dc3ab1245857c2d857f6826c
Jesse Harwin <jesse@jharwin.com>
2024-08-18 13:40:56 -0700

    added docstrings

---
+++ src/shelly/__init__.py
@@ -0,0 +1,7 @@
+"""
+`shelly` Package.
+
+This package contains modules related to interacting with Shelly devices,
+including the main control and MQTT interaction.
+"""
+from .dimmer2 import Dimmer2
